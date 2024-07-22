13649c9aceae4749446925599a35ff26eb05b7db
alan.lanceloth@gmail.com <alan.lanceloth@gmail.com>
2024-07-22 05:59:53 +0000

    feat: calculado RFV e segmentos

--- src/silver/Silver.py
+++ src/silver/Silver.py
@@ -433,16 +433,17 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Transformação
-# MAGIC
+# MAGIC # TODO
+# MAGIC - Tratar valores nulos (e também as inconsistências nos dados) [ok]
+# MAGIC - Adicionar coluna de valor total de cada transação [ok]
+# MAGIC - A partir de agregações, obter estatísticas de vendas (total de vendas por produto, por categoria, por seller)
+# MAGIC - Calcular RFV para os clientes [ok]
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC - Tratar valores nulos [ok]
-# MAGIC - Adicionar coluna de valor total de cada transação
-# MAGIC - A partir de agregações, obter estatísticas de vendas (total de vendas por produto, por categoria, por seller)
-# MAGIC - Calcular RFV para os clientes
+# MAGIC ## Limpeza de nulos e inconsistências
+# MAGIC
 
 # COMMAND ----------
 
@@ -943,6 +944,154 @@ df.drop("base_category_name").write.format("delta").mode("overwrite").saveAsTabl
 # MAGIC %sql
 # MAGIC SELECT COUNT(*) FROM olist_products
 # MAGIC WHERE NOT product_id RLIKE '^[0-9a-fA-F]{32}$'
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Agregações
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum as spark_sum
+
+# Carregar as tabelas
+df_orders = spark.read.table("olist_dataset.silver.olist_orders")
+df_order_items = spark.read.table("olist_dataset.silver.olist_order_items")
+
+# Fazer a junção das tabelas
+df_joined = df_orders.join(df_order_items, df_orders["order_id"] == df_order_items["order_id"], "left")
+
+# Calcular o valor total da venda para cada pedido
+df_total_order_value = df_joined.groupBy(df_orders["order_id"]).agg(spark_sum("price").alias("total_order_value"))
+
+# Adicionar a coluna de valor total de venda à tabela de pedidos
+df_orders_with_total = df_orders.join(df_total_order_value, "order_id", "left")
+
+# Salvar a tabela atualizada como Delta Table
+df_orders_with_total.write.format("delta").mode("overwrite").option("mergeSchema", "true").saveAsTable("olist_dataset.silver.olist_orders")
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## RFV (Recencia, Frequência e Valor)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, sum as spark_sum, count, max as spark_max, datediff, current_date
+
+# Carregar as tabelas
+df_orders = spark.read.format("delta").table("olist_dataset.silver.olist_orders")
+df_order_items = spark.read.format("delta").table("olist_dataset.silver.olist_order_items")
+df_customers = spark.read.format("delta").table("olist_dataset.silver.olist_customers")
+
+# COMMAND ----------
+
+# Filtrar os pedidos pelos status desejados
+filtered_orders = df_orders.filter(col("order_status").isin("delivered", "invoiced", "shipped", "approved"))
+
+# Obter a data da última compra de cada cliente
+df_last_purchase = filtered_orders.groupBy("customer_id").agg(spark_max("order_purchase_timestamp").alias("last_purchase_date"))
+
+# Calcular a recência
+df_recency = df_last_purchase.withColumn("recency", datediff(current_date(), col("last_purchase_date")))
+
+
+# Contar o número de pedidos de cada cliente
+df_frequency = filtered_orders.groupBy("customer_id").agg(count("order_id").alias("frequency"))
+
+# Fazer a junção das tabelas de pedidos e itens de pedido
+df_order_items_joined = df_order_items.join(filtered_orders, "order_id")
+
+# Calcular o valor total gasto por cada cliente
+df_value = df_order_items_joined.groupBy("customer_id").agg(spark_sum("price").alias("monetary_value"))
+
+# Combinar as métricas RFV
+df_rfv = df_customers.join(df_recency, "customer_id", "left")\
+    .join(df_frequency, "customer_id", "left")\
+    .join(df_value, "customer_id", "left")
+
+# Salvar a tabela RFV como Delta Table
+df_rfv.write.format("delta").mode("overwrite").saveAsTable("olist_dataset.silver.rfv")
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col, ntile, concat_ws, lit
+from pyspark.sql.window import Window
+
+# Carregar a tabela RFV
+df_rfv = spark.read.format("delta").table("olist_dataset.silver.rfv")
+
+# Definir janelas de classificação para cada métrica
+window_spec = Window.orderBy(col("recency").asc())
+df_rfv = df_rfv.withColumn("R_quartile", ntile(4).over(window_spec).cast("string"))
+
+window_spec = Window.orderBy(col("frequency").desc())
+df_rfv = df_rfv.withColumn("F_quartile", ntile(4).over(window_spec).cast("string"))
+
+window_spec = Window.orderBy(col("monetary_value").desc())
+df_rfv = df_rfv.withColumn("V_quartile", ntile(4).over(window_spec).cast("string"))
+
+# Criar um código RFV combinando as pontuações R, F e V
+df_rfv = df_rfv.withColumn("RFV_score", concat_ws("", col("R_quartile"), col("F_quartile"), col("V_quartile")))
+
+
+# Definir segmentos com base nos códigos RFV
+def segment(rfv_score, frequency):
+    if frequency is None or frequency == 0:
+        return 'Lead'
+    
+    if rfv_score in ['444', '443', '434', '433']:
+        return 'Champions'
+    elif rfv_score in ['344', '343', '334', '333','324']:
+        return 'Loyal Customers'
+    elif rfv_score in ['244', '243', '234', '233','342', '332', '322', '312','242', '232', '222', '212','224','214']:
+        return 'Potential Loyalists'
+    elif rfv_score in ['144', '143', '134', '133','142', '132', '122', '112', '114']:
+        return 'New Customers'
+    elif rfv_score in ['441', '431', '421', '411','443', '433', '423', '413']:
+        return 'Promising'
+    elif rfv_score in ['341', '331', '321', '311','343', '333', '323', '313']:
+        return 'Needs Attention'
+    elif rfv_score in ['241', '231', '221', '211','243', '233', '223', '213']:
+        return 'About to Sleep'
+    elif rfv_score in ['141', '131', '121', '111','143', '133', '123', '113']:
+        return 'At Risk'
+    elif rfv_score in ['442', '432', '422', '412']:
+        return 'Potential Champions'
+    else:
+        return 'Others'
+
+# Registrar a UDF para segmentação
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+
+segment_udf = udf(segment, StringType())
+
+# Aplicar a UDF de segmentação corretamente
+df_rfv = df_rfv.withColumn("segment", segment_udf(col("RFV_score"), col("frequency")))
+
+# Salvar a tabela RFV segmentada como Delta Table
+df_rfv.write.format("delta").mode("overwrite").option("mergeSchema", "true").saveAsTable("olist_dataset.silver.olist_customers")
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT DISTINCT segment, COUNT(DISTINCT customer_id) as customers
+# MAGIC   FROM olist_dataset.silver.olist_customers
+# MAGIC   GROUP BY segment
+# MAGIC   ORDER BY customers DESC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT *
+# MAGIC   FROM olist_dataset.silver.olist_customers
+# MAGIC   WHERE segment = "Others"
+# MAGIC   LIMIT 10;
+# MAGIC   
 
 # COMMAND ----------
 
