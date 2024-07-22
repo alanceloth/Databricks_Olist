155145b70b427afbeab0e97c4d86d8eeaa1d6b4c
alan.lanceloth@gmail.com <alan.lanceloth@gmail.com>
2024-07-22 06:18:08 +0000

    feat: agregações realizadas

--- src/silver/Silver.py
+++ src/silver/Silver.py
@@ -436,7 +436,7 @@
 # MAGIC # TODO
 # MAGIC - Tratar valores nulos (e também as inconsistências nos dados) [ok]
 # MAGIC - Adicionar coluna de valor total de cada transação [ok]
-# MAGIC - A partir de agregações, obter estatísticas de vendas (total de vendas por produto, por categoria, por seller)
+# MAGIC - A partir de agregações, obter estatísticas de vendas (total de vendas por produto, por categoria, por seller) [ok]
 # MAGIC - Calcular RFV para os clientes [ok]
 
 # COMMAND ----------
@@ -970,6 +970,59 @@ df_orders_with_total = df_orders.join(df_total_order_value, "order_id", "left")
 # Salvar a tabela atualizada como Delta Table
 df_orders_with_total.write.format("delta").mode("overwrite").option("mergeSchema", "true").saveAsTable("olist_dataset.silver.olist_orders")
 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Produtos vendidos por categoria com status (delivered, invoiced, shipped e approved)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC WITH product_counts AS (
+# MAGIC     SELECT A.product_category_name, COUNT(B.product_id) AS quantity
+# MAGIC     FROM olist_dataset.silver.olist_products AS A
+# MAGIC     LEFT JOIN olist_dataset.silver.olist_order_items AS B ON A.product_id = B.product_id
+# MAGIC     LEFT JOIN olist_dataset.silver.olist_orders O ON B.order_id = O.order_id
+# MAGIC     WHERE O.order_status IN ("delivered", "invoiced", "shipped", "approved")
+# MAGIC     GROUP BY A.product_category_name
+# MAGIC )
+# MAGIC SELECT 
+# MAGIC     product_category_name, 
+# MAGIC     quantity, 
+# MAGIC     (quantity * 100.0 / SUM(quantity) OVER ()) AS percentage_of_total
+# MAGIC FROM 
+# MAGIC     product_counts
+# MAGIC ORDER BY 
+# MAGIC     quantity DESC;
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### TOP 15 - Total de vendas por produto
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC WITH product_counts AS (
+# MAGIC     SELECT A.product_id, COUNT(B.product_id) AS quantity
+# MAGIC     FROM olist_dataset.silver.olist_products AS A
+# MAGIC     LEFT JOIN olist_dataset.silver.olist_order_items AS B ON A.product_id = B.product_id
+# MAGIC     LEFT JOIN olist_dataset.silver.olist_orders O ON B.order_id = O.order_id
+# MAGIC     --WHERE O.order_status IN ("delivered", "invoiced", "shipped", "approved")
+# MAGIC     GROUP BY A.product_id
+# MAGIC )
+# MAGIC SELECT 
+# MAGIC     product_id, 
+# MAGIC     quantity, 
+# MAGIC     (quantity * 100.0 / SUM(quantity) OVER ()) AS percentage_of_total
+# MAGIC FROM 
+# MAGIC     product_counts
+# MAGIC ORDER BY 
+# MAGIC     quantity DESC
+# MAGIC     
+# MAGIC LIMIT 15;
 
 # COMMAND ----------
 
